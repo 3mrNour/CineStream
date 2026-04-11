082d167431546199a5e4d524bae778f2af1facb5
3mrNour <amrnour1010@gmail.com>
2026-04-11 02:39:26 +0200

    feat: Add user model and authentication service for user login

---
+++ lib/data/services/auth_services.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:cinestream/data/api/api_client.dart';
+import 'package:cinestream/data/models/user_model.dart';
+import 'package:dio/dio.dart' as dio;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthServises {
+  final ApiClient _apiClient = ApiClient();
+
+  Future<UserModel?> login(
+    String username,
+    String Password,
+    BuildContext context,
+  ) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    try {
+      dio.Response loginresponse = await _apiClient.postData(
+        '/auth/login',
+        body: {"username": username, "password": Password},
+      );
+
+      if (loginresponse.statusCode == 200 && loginresponse.data != null) {
+        UserModel userdata = UserModel.fromJson(loginresponse.data);
+        prefs.setBool("isLogin", true);
+        prefs.setString("userData", jsonEncode(userdata.toJson()));
+        Navigator.pushReplacementNamed(context, '/products');
+
+        return userdata;
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return null;
+    }
+    return null;
+  }
+}
